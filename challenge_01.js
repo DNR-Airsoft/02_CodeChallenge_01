ab3ea19b93ff28da05f27780cd9575131bcb6a19
SchattenGaming <rene.otte.1991@web.de>
2022-11-09 13:56:36 +0100

    mod 2

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,24 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+"use strict";
+
+// Entscheidungsstrukturen | control flow
+
+// Deklaration + Assigment
+const ageJohn = 35;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
